1a2d3fbf5b8d54c8efcb8297943145a28a4cd0cc
dheeraj7788 <79510780+dheeraj7788@users.noreply.github.com>
2026-07-17 14:59:59 +0530

    Create save_previous.py

---
+++ scripts/save_previous.py
@@ -0,0 +1,16 @@
+"""
+No implementation required.
+
+Git automatically stores previous versions.
+
+Comparison is done using:
+
+HEAD~1
+↓
+
+HEAD
+
+No previous.yaml file is needed.
+"""
+
+print("Git history is used for comparison.")
